fb278f4f4acc573d392e7251f41302d8f841a88f
Kion <daniarkano@gmail.com>
2018-08-19 18:56:04 +0200

    Update Bean java Configuration

--- spring5-jokes-app-master/src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ spring5-jokes-app-master/src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -8,14 +8,14 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 public class JokeServiceImpl implements JokeService {
 
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
-	
-	public JokeServiceImpl() {
-		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+
+	public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+		this.chuckNorrisQuotes = chuckNorrisQuotes;
 	}
-	
+
 	@Override
 	public String getJoke() {
-		
+
 		return chuckNorrisQuotes.getRandomQuote();
 	}
 
