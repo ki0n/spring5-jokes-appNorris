da492fa3dd0984eb15ab91fe2e87c3e7dffdcc2d
Kion <daniarkano@gmail.com>
2018-08-19 14:45:28 +0200

    App jokes norris

---
+++ spring5-jokes-app-master/src/main/java/guru/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package guru.springframework.joke.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.joke.services.JokeService;
+
+@Controller
+public class JokeController {
+
+	private JokeService jokeService;
+
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/",""})
+	public String showJoke(Model model) {
+		model.addAttribute("joke", jokeService.getJoke());
+		return "checknorris";
+	}
+}
